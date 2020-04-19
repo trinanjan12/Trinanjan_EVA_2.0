907ff723d60adf605f3ffb8409ef9fdd9c3a4f8c
Trinanjan Saha <trinanjan12@gmail.com>
2020-04-20 00:01:51 +0530

    Add files via upload

--- Phase2/eva_endgame/car_env.py
+++ Phase2/eva_endgame/car_env.py
@@ -18,9 +18,13 @@ from kivy.clock import Clock
 from kivy.core.image import Image as CoreImage
 from PIL import Image as PILImage
 from kivy.graphics.texture import Texture
+import torchvision.transforms as transforms
 
 # Importing the Dqn object from our AI in ai.py
-# from ai import Dqn
+from final_t3d_new import Train_TD3
+
+
+######## Constants and Configs ########
 
 
 # Adding this line if we don't want the right click to put a red point
@@ -36,18 +40,39 @@ n_points = 0
 length = 0
 
 # Getting our AI, which we call "brain", and that contains our neural network that represents our Q-function
-# brain = Dqn(5,3,0.9)
+brain = Train_TD3()
 action2rotation = [0, 5, -5]
 last_reward = 0
 scores = []
 im = CoreImage("./images/MASK1.png")
-car_org_img = Image.open("./images/car.png").convert('L')
-car_resize_img = car_org_img.resize((20, 10))
-car_resize_img = np.asarray(car_resize_img)/255
-# textureMask = CoreImage(source="./kivytest/simplemask1.png")
+car_org_img = PILImage.open("./images/car.png").convert('RGBA')
+car_org_img = car_org_img.resize((20, 10))
+sand_img = PILImage.open("./images/mask.png").convert('L')
 
 # Initializing the map
 first_update = True
+
+# Initializing the last distance
+last_distance = 0
+total_reward = 0
+
+######## Helper Methods ########
+
+
+def get_input_image(x, y, angle):
+    x, y, angle = int(y), int(x), int(angle)
+    # x, y, angle = 80, 80, 45
+    car_rotated = car_org_img.rotate(angle, expand=1)
+    sand_img_copy = sand_img.copy()
+    sand_img_copy.paste(car_rotated, (x, y), car_rotated)
+
+    img_patch = sand_img_copy.crop((x-30, y-30, x+30, y+30))
+    img_patch = img_patch.convert('L')
+    # img_patch.save('./output_patch/car.png')
+    # sand_img_copy.save('./output_patch/sand.png')
+    img_patch = np.array(img_patch) / 255
+    # print(img_patch.min(),img_patch.max())
+    return np.expand_dims(img_patch, axis=0)
 
 
 def init():
@@ -65,10 +90,7 @@ def init():
     swap = 0
 
 
-# Initializing the last distance
-last_distance = 0
-
-# car DE Player
+########## Car Agent ##########
 
 
 class Car(Widget):
@@ -78,58 +100,18 @@ class Car(Widget):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
-    # sensor1_x = NumericProperty(0)
-    # sensor1_y = NumericProperty(0)
-    # sensor1 = ReferenceListProperty(sensor1_x, sensor1_y)
-    # sensor2_x = NumericProperty(0)
-    # sensor2_y = NumericProperty(0)
-    # sensor2 = ReferenceListProperty(sensor2_x, sensor2_y)
-    # sensor3_x = NumericProperty(0)
-    # sensor3_y = NumericProperty(0)
-    # sensor3 = ReferenceListProperty(sensor3_x, sensor3_y)
-    # signal1 = NumericProperty(0)
-    # signal2 = NumericProperty(0)
-    # signal3 = NumericProperty(0)
 
     def move(self, rotation):
         self.pos = Vector(*self.velocity) + self.pos
         self.rotation = rotation
         self.angle = self.angle + self.rotation
-        # self.sensor1 = Vector(30, 0).rotate(self.angle) + self.pos
-        # self.sensor2 = Vector(30, 0).rotate((self.angle+30)%360) + self.pos
-        # self.sensor3 = Vector(30, 0).rotate((self.angle-30)%360) + self.pos
-        # self.signal1 = int(np.sum(sand[int(self.sensor1_x)-10:int(self.sensor1_x)+10, int(self.sensor1_y)-10:int(self.sensor1_y)+10]))/400.
-        # self.signal2 = int(np.sum(sand[int(self.sensor2_x)-10:int(self.sensor2_x)+10, int(self.sensor2_y)-10:int(self.sensor2_y)+10]))/400.
-        # self.signal3 = int(np.sum(sand[int(self.sensor3_x)-10:int(self.sensor3_x)+10, int(self.sensor3_y)-10:int(self.sensor3_y)+10]))/400.
-        # if self.sensor1_x>longueur-10 or self.sensor1_x<10 or self.sensor1_y>largeur-10 or self.sensor1_y<10:
-        #     self.signal1 = 10.
-        # if self.sensor2_x>longueur-10 or self.sensor2_x<10 or self.sensor2_y>largeur-10 or self.sensor2_y<10:
-        #     self.signal2 = 10.
-        # if self.sensor3_x>longueur-10 or self.sensor3_x<10 or self.sensor3_y>largeur-10 or self.sensor3_y<10:
-        #     self.signal3 = 10.
-# ball DE sensors
 
-
-class Ball1(Widget):
-    pass
-
-
-class Ball2(Widget):
-    pass
-
-
-class Ball3(Widget):
-    pass
-
-# map DE Env
+########## Car ENV ##########
 
 
 class Game(Widget):
 
     car = ObjectProperty(None)
-    # ball1 = ObjectProperty(None)
-    # ball2 = ObjectProperty(None)
-    # ball3 = ObjectProperty(None)
 
     def serve_car(self):
         self.car.center = self.center
@@ -146,45 +128,63 @@ class Game(Widget):
         global longueur
         global largeur
         global swap
+        global total_reward
 
+        # Width and height of the entire map 1429x660
         longueur = self.width
         largeur = self.height
-
+        # set and load initial values
         if first_update:
             init()
-        print(largeur, longueur)
-        print(type(sand), sand.shape())
 
+        episode_done = False
+        # distance in x and y from the goal
+        # using this cordinate we can calculate the angle to move towards the target
         xx = goal_x - self.car.x
         yy = goal_y - self.car.y
+
+        # orientation gives you angle value towards direction
+        # ?????????? basically orientation should be a angle between 2 points end , current
+        ########## orientation = Vector(*self.car.pos).angle((goal_x,goal_y))/180.
         orientation = Vector(*self.car.velocity).angle((xx, yy))/180.
+
         # last_signal = [self.car.signal1, self.car.signal2, self.car.signal3, orientation, -orientation]
+        # # networks predicts action which helps the cat to move
+        # # rotation changes the car angle thus changes the position of the car
         # action = brain.update(last_reward, last_signal)
         # scores.append(brain.score())
-        action = np.random.randint(0, 3)
-        rotation = action2rotation[action]
-        self.car.move(rotation)
-        distance = np.sqrt((self.car.x - goal_x)**2 + (self.car.y - goal_y)**2)
-        # self.ball1.pos = self.car.sensor1
-        # self.ball2.pos = self.car.sensor2
-        # self.ball3.pos = self.car.sensor3
+        # rotation = action2rotation[action]
 
+        # Input to the network
+        last_signal = [[np.array(get_input_image(self.car.x, self.car.y, self.car.angle)), orientation, -orientation], episode_done]
+        # Output from the network
+        action = brain.update(last_reward, last_signal)
+        # scores.append(brain.score())
+        # action = np.random.randint(0, 3)
+        rotation = int(action[0])
+        # print(action)
+        self.car.move(rotation)
+        # the distance for final goal
+        distance = np.sqrt((self.car.x - goal_x)**2 + (self.car.y - goal_y)**2)
+
+        # if the car position is on sand
         if sand[int(self.car.x), int(self.car.y)] > 0:
             self.car.velocity = Vector(0.5, 0).rotate(self.car.angle)
-            print(1, goal_x, goal_y, distance, int(self.car.x), int(
-                self.car.y), im.read_pixel(int(self.car.x), int(self.car.y)))
+            # print(1, goal_x, goal_y, distance, int(self.car.x), int(
+            #     self.car.y), im.read_pixel(int(self.car.x), int(self.car.y)))
 
             last_reward = -1
+
+        # else car is on the road
         else:  # otherwise
             self.car.velocity = Vector(2, 0).rotate(self.car.angle)
             last_reward = -0.2
-            print(0, goal_x, goal_y, distance, int(self.car.x), int(
-                self.car.y), im.read_pixel(int(self.car.x), int(self.car.y)))
+            # print(0, goal_x, goal_y, distance, int(self.car.x), int(
+            #     self.car.y), im.read_pixel(int(self.car.x), int(self.car.y)))
             if distance < last_distance:
                 last_reward = 0.1
-            # else:
-            #     last_reward = last_reward +(-0.2)
 
+        # reset car if the car is near the wall
         if self.car.x < 5:
             self.car.x = 5
             last_reward = -1
@@ -198,35 +198,27 @@ class Game(Widget):
             self.car.y = self.height - 5
             last_reward = -1
 
-        if distance < 25:
-            if swap == 1:
-                goal_x = 1420
-                goal_y = 622
-                swap = 0
-            else:
-                goal_x = 9
-                goal_y = 85
-                swap = 1
+        # check if the car has reached the goal and swap
+        if distance < 25 or total_reward < -2000:
+            episode_done = True
+            total_reward = 0
+            self.car.x = int(np.random.randint(25, self.width-25, 1)[0])
+            self.car.y = int(np.random.randint(25, self.height-25, 1)[0])
+            goal_x = int(np.random.randint(25, self.width-25, 1)[0])
+            goal_y = int(np.random.randint(25, self.width-25, 1)[0])
+            print("new episode goals", (self.car.x, self.car.y), (goal_x, goal_y))
+            # if swap == 1:
+            #     goal_x = int(np.random.randint(25, self.width-25, 1)[0])
+            #     goal_y = int(np.random.randint(25, self.width-25, 1)[0])
+            #     swap = 0
+            # else:
+            #     goal_x = int(np.random.randint(25, self.width-25, 1)[0])
+            #     goal_y = int(np.random.randint(25, self.width-25, 1)[0])
+            #     swap = 1
         last_distance = distance
+        total_reward += last_reward
 
-    # Crop sand with car image patch for T3D CNN Input
-    def get_image(self):
-        patch_size = 40
-        car_width = 20
-        car_height = 10
-
-        # sand has the dimension of 660X1429
-        env_render_image = sand.copy()  # creates a copy of sand variable
-        # places the car image on the map  (transpose because the sand image is vertical)
-        env_render_image[self.car.x - int(car_width/2):self.car.x + int(car_width/2), self.car.x - int(
-            car_height/2):self.car.x + int(car_height/2)] = np.transpose(car_resize_img)
-        # Crops a patch of image with a patch size
-        env_render_image_patch = env_render_image[self.car.x - (int(car_height/2) + patch_size):self.car.x + (int(
-            car_height/2) + patch_size), self.car.x - (int(car_height/2) + patch_size):self.car.x + (int(car_height/2) + patch_size)]
-
-        env_render_image_patch = PILImage.fromarray(env_render_image_patch, 'RGB')  # return image patch
-
-        return env_render_image_patch
+######### Adding the painting tools #########
 
 
 class MyPaintWidget(Widget):
@@ -261,7 +253,7 @@ class MyPaintWidget(Widget):
             last_x = x
             last_y = y
 
-# Running the whole thing
+######### Running the whole thing #########
 
 
 class CarApp(App):
@@ -289,15 +281,14 @@ class CarApp(App):
 
     def save(self, obj):
         print("saving brain...")
-        brain.save()
+        # brain.save()
         plt.plot(scores)
         plt.show()
 
     def load(self, obj):
         print("loading last saved brain...")
-        brain.load()
+        # brain.load()
 
 
-# Running the whole thing
 if __name__ == '__main__':
     CarApp().run()
